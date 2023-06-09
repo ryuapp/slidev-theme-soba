4c525c697568b02d9d18c9d40a487b77b1d2d558
ryu <114303361+ryuapp@users.noreply.github.com>
2023-06-10 04:27:04 +0900

    ✨ Add page count component

---
+++ global-top.vue
@@ -0,0 +1,8 @@
+<template>
+    <header 
+        v-if="$slidev.configs.showPageCount && !$slidev.configs.disablePageCount?.includes($slidev.nav.currentPage)"
+        class="absolute top-0 right-0 pt-2 p-5 font-serif text-lg"
+    >
+        <SlideCurrentNo />
+    </header>
+</template>
