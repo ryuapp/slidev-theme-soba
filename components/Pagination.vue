f1b13443f0f0ee3a61c3d4bfd0da8d05faec4d47
ryu <114303361+ryuapp@users.noreply.github.com>
2023-06-11 23:45:54 +0900

    💄 Page count component to slide bottom

---
+++ components/Pagination.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    v-if="$slidev.configs.showPageCount && !$slidev.configs.disablePageCount?.includes($slidev.nav.currentPage)"
+    class="font-serif text-lg"
+  >
+    <SlideCurrentNo />
+  </div>
+</template>
---
+++ global-bottom.vue
@@ -0,0 +1,10 @@
+import Pagination from './components/Pagination.vue';
+
+<template>
+    <footer class="absolute bottom-0 pb-1 px-3 w-full">
+        <div class="flex justify-between w">
+            <div></div>
+            <Pagination />
+        </div>
+    </footer>
+</template>
