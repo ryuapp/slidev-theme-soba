3511e99c7966018f0bef21aea4950e5af97b3740
ryu <114303361+ryuapp@users.noreply.github.com>
2024-07-14 20:59:53 +0900

    💄 Update code block style

---
+++ setup/shiki.ts
@@ -0,0 +1,10 @@
+import type { ShikiSetupReturn } from '@slidev/types'
+import { defineShikiSetup } from '@slidev/types'
+
+export default defineShikiSetup((): ShikiSetupReturn => {
+  return {
+    themes: {
+      light: 'github-dark',
+    },
+  }
+})
